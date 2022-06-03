76cf12eba76acd0b4be02adb86aebfafe652eec1
Trevor Edwards <trevor.jeffrey.edwards@gmail.com>
2022-06-03 09:09:07 -0400

    Movement Working

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,19 +6,28 @@ public class PlayerMovement : MonoBehaviour
 {
     public float speed;
     private Rigidbody2D myRigidbody;
-    private Vector2 change;
-    // Start is called before the first frame update
+    private Vector3 change;
+
     void Start()
     {
         myRigidbody = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        change = Vector2.zero;
+        change = Vector3.zero;
         change.x = Input.GetAxisRaw("Horizontal");
         change.y = Input.GetAxisRaw("Vertical");
-        Debug.Log(change);
+        if(change != Vector3.zero)
+        {
+            MoveCharacter();
+        }
+    }
+
+    void MoveCharacter()
+    {
+        myRigidbody.MovePosition(
+            transform.position + change * speed * Time.deltaTime
+        );
     }
 }
