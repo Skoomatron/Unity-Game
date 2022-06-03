e7f425f78ddad449b7521ea5816803b5d86ae20f
Trevor Edwards <trevor.jeffrey.edwards@gmail.com>
2022-06-03 08:45:56 -0400

    PlayerMovement Script Implemented

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float speed;
+    private Rigidbody2D myRigidbody;
+    private Vector2 change;
     // Start is called before the first frame update
     void Start()
     {
-        
+        myRigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        change = Vector2.zero;
+        change.x = Input.GetAxisRaw("Horizontal");
+        change.y = Input.GetAxisRaw("Vertical");
+        Debug.Log(change);
     }
 }
